8b70baa40df2b204af21e10dd339fc214d72b28a
André dos Santos <adesantosa@gmail.com>
2021-04-06 17:57:28 -0300

    first commit

---
+++ src/main/kotlin/com/study/andsantos/WebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.study.andsantos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GettingStartedWebfluxSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GettingStartedWebfluxSampleApplication>(*args)
+}
